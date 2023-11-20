fb0870477ac81a533367dfb102e2a61c0ac036e1
Lishanth <120487924+Lishanth007@users.noreply.github.com>
2023-11-20 09:48:04 +0530

    My first Spring app registration form

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.User;
+
+@Controller
+public class IndexController {
+
+    // Mapping for the root directory
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    // Mapping for the registration form
+    @PostMapping("/register")
+	public String regForm(@ModelAttribute User user,Model mode ) {
+		
+		System.out.println(user.toString());
+		
+		//addAttribute(), It transfers the name/value to another file,
+		//It takes two parameters(String attributeName,Object AttributeName)
+		mode.addAttribute("firstname", user.getFname());
+		mode.addAttribute("lastname", user.getLname());
+		return "welcome";
+	}
+    
+    @RequestMapping("/find")
+    public String newFile() {
+        return "course";
+    } 
+    
+    @RequestMapping("/findjava")
+    public String javacourse() {
+        return "javaCourse";
+    } 
+    
+}
